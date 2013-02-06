0a772c9924822c580f016abe8c657aaad9e017e3
GianniRossini <magiainformatica@alice.it>
2013-02-06 18:32:12 +0100

    added license info for document class

--- wxsagra.py
+++ wxsagra.py
@@ -18,8 +18,35 @@ import calendar
 import datetime
 from collections import OrderedDict
 
+# from MSWinPrint I used class document 
+#  here is license from original MSwinPrint
 
-# from MSWinPrint
+"""
+MSWinPrint.py
+Copyright 2006-2012 Chris Gonnerman.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer. Redistributions in binary
+form must reproduce the above copyright notice, this list of conditions and
+the following disclaimer in the documentation and/or other materials
+provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+"""
+
 import win32gui, win32ui, win32print, win32con
 import Image
 #import pprint
@@ -426,6 +453,9 @@ def save_tabbed_file(filename,portate):
     fp.close 
     return
 
+# from original work MSWinPrint.py
+# Copyright 2006-2012 Chris Gonnerman.
+# see license in first line (20 ... ) of this module 
 class document:
 
     def __init__(self, printer = None, papersize = None, orientation = None):
